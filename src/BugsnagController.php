f2b02601e3d1c904f725c5c45cee0df68e8adc64
Svenvdzwet <s.vanderzwet@student.avans.nl>
2022-09-13 13:29:59 +0200

    Fixed the manual command to send release version to Bugsnag.

--- src/BugsnagController.php
+++ src/BugsnagController.php
@@ -2,7 +2,9 @@
 
 namespace Violet88\BugsnagModule;
 
+use Psr\Container\NotFoundExceptionInterface;
 use SilverStripe\Control\Controller;
+use SilverStripe\Control\HTTPResponse;
 use SilverStripe\Core\Injector\Injector;
 
 class BugsnagController extends Controller
@@ -11,13 +13,24 @@ class BugsnagController extends Controller
         'index'
     ];
 
-    public function index()
+    /**
+     * @throws NotFoundExceptionInterface
+     */
+    public function index(): HTTPResponse
     {
         $repository = $_GET['repository'] ?? null;
         $revision = $_GET['revision'] ?? null;
         $provider = $_GET['provider'] ?? null;
         $builderName = $_GET['builderName'] ?? null;
+        $appVersion = $_GET['appVersion'] ?? null;
 
-        Injector::inst()->get(Bugsnag::class)->notifyBuild($repository, $revision, $provider, $builderName);
+        $bugsnag = Injector::inst()->get(Bugsnag::class);
+        $bugsnag
+            ->addAppVersion($appVersion)
+            ->notifyBuild($repository, $revision, $provider, $builderName);
+
+        $response = new HTTPResponse();
+        $response->setStatusCode(200);
+        return $response;
     }
 }
