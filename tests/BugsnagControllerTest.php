41f75ae1c71d80c2296dc1309b2eed2b09816fbc
Svenvdzwet <s.vanderzwet@student.avans.nl>
2022-09-19 11:49:16 +0200

    Added test for initial sending CLI command. Also changed the test to use mocks, so no messages will be sent to Bugsnag while running tests.

--- tests/BugsnagControllerTest.php
+++ tests/BugsnagControllerTest.php
@@ -2,7 +2,13 @@
 
 namespace Violet88\BugsnagModule\Tests;
 
+use Bugsnag\Client;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Environment;
+use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Dev\FunctionalTest;
+use Violet88\BugsnagModule\Bugsnag;
+use Violet88\BugsnagModule\TestSnag;
 
 /**
  * @covers \Violet88\BugsnagModule\BugsnagController
@@ -12,8 +18,39 @@ class BugsnagControllerTest extends FunctionalTest
 {
     public function testCommand()
     {
-        $response = $this->get('bugsnag_build');
+        Injector::nest();
+
+        $testSnag = $this->getMockBuilder(Bugsnag::class)
+            ->setMethods(['notifyBuild', 'setAppVersion'])
+            ->getMock();
+
+        $testSnag->expects($this->once())
+            ->method('notifyBuild')
+            ->willReturnSelf();
+        $testSnag->expects($this->once())
+            ->method('setAppVersion')
+            ->willReturnSelf();
+
+        Injector::inst()->registerService($testSnag, Bugsnag::class);
+        $response = $this->get('bugsnag/build');
         $this->assertEquals(200, $response->getStatusCode());
+
+        Injector::unnest();
     }
 
+    public function testInitial()
+    {
+        Injector::nest();
+
+        //Making a 'mock' client so no useless errors are sent to Bugsnag.
+        $testSnag = $this->getMockBuilder(Bugsnag::class)
+            ->setMethods(['sendException'])
+            ->getMock();
+
+        Injector::inst()->registerService($testSnag, Bugsnag::class);
+        $response = $this->get('bugsnag/initial');
+        $this->assertEquals(200, $response->getStatusCode());
+
+        Injector::unnest();
+    }
 }
