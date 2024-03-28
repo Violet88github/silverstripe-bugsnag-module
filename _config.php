7be075a006dc06ab90fad838c7dce4371d567a98
RheimerViolet88 <105431098+RheimerViolet88@users.noreply.github.com>
2024-03-28 14:24:17 +0100

    Fixed routes and "leftAndMain' menu item

--- _config.php
+++ _config.php
@@ -1,4 +1,9 @@
 <?php
 
+use SilverStripe\Admin\CMSMenu;
+use Violet88\BugsnagModule\BugsnagSiteConfigExtension_Controller;
+
+CMSMenu::remove_menu_class(BugsnagSiteConfigExtension_Controller::class);
+
 // You need this file if you don't have anything in the _config folder. If that folder exists
 // and is not empty then you can delete this file.
